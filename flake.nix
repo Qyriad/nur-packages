b967c1264b637cdb9f2c52fb62b2e86bbe06a564
Qyriad <qyriad@qyriad.me>
2024-02-04 17:44:35 -0700

    flake: don't try to eval on unsupported systems

--- flake.nix
+++ flake.nix
@@ -15,8 +15,15 @@
           inherit pkgs;
         };
 
+        # We need to filter out attrsets that aren't derivations,
+        # like the functors added by callPackage.
+        # And then we also want to filter out packages that aren't available
+        # on the system we're evaluating for.
+        isDrvAndAvail = drv:
+          lib.isDerivation drv && lib.meta.availableOn pkgs.stdenv.hostPlatform drv;
+
       in {
-        packages = lib.filterAttrs (name: value: lib.isDerivation value) nurPackages;
+        packages = lib.filterAttrs (name: value: isDrvAndAvail value) nurPackages;
       }
 
     ) # eachDefaultSystem
