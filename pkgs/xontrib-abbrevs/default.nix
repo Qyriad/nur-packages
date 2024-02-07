516dd229101155a1f9e0468a93681eea5391b6ff
Qyriad <qyriad@qyriad.me>
2024-02-06 18:17:00 -0700

    xontrib-abbrevs: compatibility with NixOS/nixpkgs#282368 (a30ae78435b7c481233601eea523b9340ca0760f).

--- pkgs/xontrib-abbrevs/default.nix
+++ pkgs/xontrib-abbrevs/default.nix
@@ -1,7 +1,7 @@
 {
   lib,
   python3,
-  xonsh-unwrapped,
+  xonsh,
   fetchFromGitHub,
 }:
 
@@ -13,6 +13,27 @@ let
     poetry-core
     prompt-toolkit
   ;
+
+  # Compatibility with NixOS/nixpkgs#282368 (a30ae78435b7c481233601eea523b9340ca0760f).
+  # The wrapper breaks importing xonsh as a library, which means it won't work in
+  # nativeCheckInputs. pkgs.xonsh-unwrapped was removed in the above PR, so to access
+  # the underlying xonsh library we call overridePythonAttrs to get the arguments
+  # buildPythonApplication was originally called with, and then call buildPythonApplication
+  # again with those arguments.
+  xonshUnwrapped = let
+    # Make a version of xonsh that has the original buildPythonApplication args
+    # as a `passthru` attr.
+    withPassthruArgs = xonsh.overridePythonAttrs (self: {
+      passthru = self.passthru // {
+        pythonAttrs = self;
+      };
+    });
+
+    # Then get that attribute,
+    pythonArgs = withPassthruArgs.pythonAttrs;
+  in
+    # And pass it as buildPythonApplication args.
+    python3.pkgs.buildPythonApplication pythonArgs;
 
   pname = "xontrib-abbrevs";
   version = "0.0.1";
@@ -37,7 +58,7 @@ in
     ];
 
     nativeCheckInputs = [
-      xonsh-unwrapped
+      xonshUnwrapped
       prompt-toolkit
     ];
 
