8cb49d41ba42ef6009ef7050ab7a9cb5f2406705
Qyriad <qyriad@qyriad.me>
2024-03-16 22:03:02 -0600

    flake: decrease indentation

--- flake.nix
+++ flake.nix
@@ -5,29 +5,25 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+    flake-utils.lib.eachDefaultSystem (system: let
 
-        pkgs = import nixpkgs { inherit system; };
-        inherit (pkgs) lib;
+      pkgs = import nixpkgs { inherit system; };
+      inherit (pkgs) lib;
 
-        nurPackages = import ./default.nix {
-          inherit pkgs;
-        };
+      nurPackages = import ./default.nix {
+        inherit pkgs;
+      };
 
-        # We need to filter out attrsets that aren't derivations,
-        # like the functors added by callPackage.
-        # And then we also want to filter out packages that aren't available
-        # on the system we're evaluating for.
-        isDrvAndAvail = drv:
-          lib.isDerivation drv && lib.meta.availableOn pkgs.stdenv.hostPlatform drv;
+      # We need to filter out attrsets that aren't derivations,
+      # like the functors added by callPackage.
+      # And then we also want to filter out packages that aren't available
+      # on the system we're evaluating for.
+      isDrvAndAvail = drv:
+        lib.isDerivation drv && lib.meta.availableOn pkgs.stdenv.hostPlatform drv;
 
-      in {
-        packages = lib.filterAttrs (name: value: isDrvAndAvail value) nurPackages;
-        checks = self.packages.${system};
-      }
-
-    ) # eachDefaultSystem
-
-  ;# outputs
+    in {
+      packages = lib.filterAttrs (name: value: isDrvAndAvail value) nurPackages;
+      checks = self.packages.${system};
+    })
+  ; # outputs
 }
