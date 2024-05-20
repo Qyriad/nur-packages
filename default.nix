8397ea2ba5b7e748ce9e0060d14104c1ec28bdc7
Qyriad <qyriad@qyriad.me>
2024-05-20 07:49:48 -0600

    migrate to by-name packages

--- default.nix
+++ default.nix
@@ -1,5 +1,9 @@
 {
   pkgs ? import <nixpkgs> { },
-}:
+}: let
+  inherit (pkgs) lib;
 
-pkgs.callPackage ./pkgs { }
+in lib.makeScope pkgs.newScope (self: lib.packagesFromDirectoryRecursive {
+  callPackage = self.callPackage;
+  directory = ./pkgs;
+})
