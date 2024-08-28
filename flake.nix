218d50481c7aa81d710340f7f251f6bd9522289a
Qyriad <qyriad@qyriad.me>
2024-08-28 11:14:35 -0600

    re-format flake.nix

--- flake.nix
+++ flake.nix
@@ -4,30 +4,32 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: flake-utils.lib.eachDefaultSystem (system: let
 
-      pkgs = import nixpkgs { inherit system; };
-      inherit (pkgs) lib;
+    pkgs = import nixpkgs { inherit system; };
+    inherit (pkgs) lib;
 
-      nurPackages = import ./default.nix {
-        inherit pkgs;
-      };
+    nurPackages = import ./default.nix {
+      inherit pkgs;
+    };
 
-      nurLib = import ./lib { inherit (pkgs) lib; };
-      isAvailableDerivation = nurLib.isAvailableDerivation pkgs.stdenv.hostPlatform;
+    nurLib = import ./lib { inherit (pkgs) lib; };
+    isAvailableDerivation = nurLib.isAvailableDerivation pkgs.stdenv.hostPlatform;
 
-      # We need to filter out attrsets that aren't derivations,
-      # like the functors added by callPackage.
-      # And then we also want to filter out packages that aren't available
-      # on the system we're evaluating for.
-      packages = lib.filterAttrs (lib.const isAvailableDerivation) nurPackages;
+    # We need to filter out attrsets that aren't derivations,
+    # like the functors added by callPackage.
+    # And then we also want to filter out packages that aren't available
+    # on the system we're evaluating for.
+    packages = lib.filterAttrs (lib.const isAvailableDerivation) nurPackages;
 
-    in {
-      packages = packages // {
-        default = pkgs.linkFarmFromDrvs "qyriad-nur" (lib.attrValues packages);
-      };
-      checks = self.packages.${system};
-    })
-  ; # outputs
+  in {
+    packages = packages // {
+      default = pkgs.linkFarmFromDrvs "qyriad-nur" (lib.attrValues packages);
+    };
+    checks = self.packages.${system};
+  }); # outputs
 }
