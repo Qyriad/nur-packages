40f3687956da8a76ff43ca0c2e1dd411564ef6da
Qyriad <qyriad@qyriad.me>
2025-03-01 16:32:21 +0100

    slightly refactor default.nix

--- default.nix
+++ default.nix
@@ -1,26 +1,24 @@
 {
   pkgs ? import <nixpkgs> { },
   lib ? pkgs.lib,
-}:
-
-let
-
-  # Uses `self` as the scope to `callPackage` everything in `./pkgs`.
-  makePackages = self: lib.packagesFromDirectoryRecursive {
-    callPackage = self.callPackage;
-    directory = ./pkgs;
-  };
+}: lib.makeScope pkgs.newScope (self: let
+  # Make our recursive scope, which contains packages auto-discovered
+  # from `./pkgs`, as well as our extended `lib`, meaning those will all be
+  # usable in things `callPackage`d in this scope, and, since we're using
+  # `pkgs` as our parent scope, things from `pkgs` will also be available in
+  # the same way.
 
   lib' = lib.extend (final: prev: import ./lib {
     lib = final;
   });
 
-  # Finally, make our recursive scope, which contains packages auto-discovered
-  # from `./pkgs`, as well as our extended `lib`, meaning those will all be
-  # usable in things `callPackage`d in this scope, and, since we're using
-  # `pkgs` as our parent scope, things from `pkgs` will also be available in
-  # the same way.
-in lib.makeScope pkgs.newScope (self: makePackages self // {
+  # Uses `self` as the scope to `callPackage` everything in `./pkgs`.
+  discoveredPackages = lib.packagesFromDirectoryRecursive {
+    callPackage = self.callPackage;
+    directory = ./pkgs;
+  };
+
+in discoveredPackages // {
   lib = lib';
 
   # For exploration purposes.
@@ -38,7 +36,7 @@ in lib.makeScope pkgs.newScope (self: makePackages self // {
   # For exploration purposes.
   availablePackages = let
     isAvailable = lib'.isAvailableDerivation pkgs.stdenv.hostPlatform;
-  in lib.filterAttrs (lib.const isAvailable) (makePackages self);
+  in lib.filterAttrs (lib.const isAvailable) discoveredPackages;
 
   mkAbsoluteDylibsHook = self.callPackage ./helpers/absolute-dylibs.nix { };
 
