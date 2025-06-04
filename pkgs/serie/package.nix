d071d9dd99e386486357c3cf2b026cc22c0f844c
Qyriad <qyriad@qyriad.me>
2025-06-04 12:38:14 +0200

    serie: 0.4.4 -> 0.4.6
    Diff: https://github.com/lusingander/serie/compare/refs/tags/v0.4.4...refs/tags/v0.4.6

--- pkgs/serie/package.nix
+++ pkgs/serie/package.nix
@@ -17,7 +17,7 @@
   ;
 in stdenv.mkDerivation (self: {
   pname = "serie";
-  version = "0.4.4";
+  version = "0.4.6";
 
   strictDeps = true;
   __structuredAttrs = true;
@@ -27,13 +27,13 @@ in stdenv.mkDerivation (self: {
     owner = "lusingander";
     repo = "serie";
     rev = "refs/tags/v${self.version}";
-    hash = "sha256-Uf7HYcN/lJc2TSl2dZQcOKyEeLHMb2RTQwSzXWZnBkw=";
+    hash = "sha256-26B/bwXz60fcZrh6H1RPROiML44S1Pt1J3VrJh2gRrI=";
   };
 
   cargoDeps = fetchCargoVendor {
     name = "${self.finalPackage.name}-cargo-deps";
     inherit (self) src;
-    hash = "sha256-NbBF747sSxmjlTbcYknNZFFsaIVZ6+wHhjMJ6akg4BU=";
+    hash = "sha256-Bdk553tECJiMxJlXj147Sv2LzH+nM+/Cm5BpBr78I4o=";
   };
 
   nativeBuildInputs = rustHooks.asList ++ [
