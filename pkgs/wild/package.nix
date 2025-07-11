77992317d7e2026ed3a4087f20c12706f5595cd5
Qyriad <qyriad@qyriad.me>
2025-07-11 15:25:47 +0200

    wild: 0.2.0 -> 0.5.0
    Diff: https://github.com/davidlattimore/wild/compare/refs/tags/0.2.0...refs/tags/0.5.0

--- pkgs/wild/package.nix
+++ pkgs/wild/package.nix
@@ -12,7 +12,7 @@
   cargoInstallHook,
 }: stdenv.mkDerivation (self: {
   pname = "wild";
-  version = "0.2.0";
+  version = "0.5.0";
 
   strictDeps = true;
   __structuredAttrs = true;
@@ -21,13 +21,13 @@
     owner = "davidlattimore";
     repo = "wild";
     rev = "refs/tags/${self.version}";
-    hash = "sha256-07fBScwJc7vmGIkKVMuatz8EMq5wc5ISpLPt7FTIR6g=";
+    hash = "sha256-tVGvSd4aege3xz/CrEl98AwuEJlsM3nVVG0urTSajFQ=";
   };
 
   cargoDeps = fetchCargoVendor {
     name = "${self.finalPackage.name}-cargo-deps";
     inherit (self) src;
-    hash = "sha256-mL1n4fDbr8SAyVOLk5wyYhjbh0mWqLXPx0xWXu/4Wp8=";
+    hash = "sha256-dXIYJfjz6okiLJuX4ZHu0Ft2/9XDjCrvvl/eqeuvBkU=";
   };
   cargoBuildType = "release";
 
