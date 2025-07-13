d2e6eb7ec1d427ac33f2ec277a404b153f1b2147
Qyriad <qyriad@qyriad.me>
2025-07-13 21:29:45 +0200

    lib: split and pub use ./derivations.nix::*

--- lib/default.nix
+++ lib/default.nix
@@ -3,20 +3,6 @@
 }: let
   inherit (lib) typeOf;
 
-  /**
-    Return true if and only if `drv` is a derivation which is available on the
-    given platform and not broken.
-  */
-  isAvailableDerivation = hostPlatform: drv: lib.all lib.trivial.id [
-    (lib.isDerivation drv)
-    (lib.meta.availableOn hostPlatform drv)
-    (drv.meta.broken or null != true)
-  ];
-
-  isEvalableDerivation = drv: let
-    res = builtins.tryEval (drv.outPath or (throw ""));
-  in res.success && lib.isDerivation drv;
-
   /** Takes the result of a `builtins.tryEval` invocation, and a fallback value.
    * If the `tryEval` succeeded, return its value. Otherwise, return `fallback`.
    */
@@ -119,17 +105,8 @@
   /** Given a function to apply to, splat a list as application arguments. */
   applyTo = f: argList: apply argList f;
 
-  isScope = attrs: lib.all (value: value == true) [
-    (lib.isFunction attrs.callPackage or null)
-    (lib.isFunction attrs.newScope or null)
-    (lib.isFunction attrs.overrideScope or null)
-    (lib.isFunction attrs.packages or null)
-  ];
-
 in {
   inherit
-    isAvailableDerivation
-    isEvalableDerivation
     tryResOr
     optionalDefault
     mkPlatformPredicates
@@ -143,6 +120,7 @@ in {
     foldlAttrsToList'
     apply
     applyTo
-    isScope
   ;
 }
+# pub use ./derivations.nix::*;
+// import ./derivations.nix { }
---
+++ lib/derivations.nix
@@ -0,0 +1,33 @@
+{
+  lib ? import ./default.nix { },
+}: let
+
+  /**
+    Return true if and only if `drv` is a derivation which is available on the
+    given platform and not broken.
+  */
+  isAvailableDerivation = hostPlatform: drv: lib.all lib.trivial.id [
+    (lib.isDerivation drv)
+    (lib.meta.availableOn hostPlatform drv)
+    (drv.meta.broken or null != true)
+  ];
+
+  isEvalableDerivation = drv: let
+    res = builtins.tryEval (drv.outPath or (throw ""));
+  in res.success && lib.isDerivation drv;
+
+  isScope = attrs: lib.all (value: value == true) [
+    (lib.isFunction attrs.callPackage or null)
+    (lib.isFunction attrs.newScope or null)
+    (lib.isFunction attrs.overrideScope or null)
+    (lib.isFunction attrs.packages or null)
+  ];
+
+
+in {
+  inherit
+    isAvailableDerivation
+    isEvalableDerivation
+    isScope
+  ;
+}
