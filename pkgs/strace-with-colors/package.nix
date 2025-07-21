777e9bf658f85ab74ea9c42bb6fe796aec67f7de
Qyriad <qyriad@qyriad.me>
2025-07-21 18:34:23 +0200

    strace-with-colors: allow unstructured

--- pkgs/strace-with-colors/package.nix
+++ pkgs/strace-with-colors/package.nix
@@ -1,15 +1,18 @@
 {
-  lib,
   strace,
   fetchpatch,
 }:
 
 # Strace patched to have colored output.
 strace.overrideAttrs (prev: {
-  patches = prev.patches or [ ] ++ lib.singleton (fetchpatch {
-    url = "https://raw.githubusercontent.com/xfgusta/strace-with-colors/v6.3-1/strace-with-colors.patch";
-    hash = "sha256-gcQldGsRgvGnrDX0zqcLTpEpchNEbCUFdKyii0wetEI=";
-  });
+  patches = let
+    colorPatch = fetchpatch {
+      url = "https://raw.githubusercontent.com/xfgusta/strace-with-colors/v6.3-1/strace-with-colors.patch";
+      hash = "sha256-gcQldGsRgvGnrDX0zqcLTpEpchNEbCUFdKyii0wetEI=";
+    };
+  in prev.patches or [ ] ++ [ colorPatch ];
+
+  passthru.allowUnstructuredAttrs = true;
 
   meta = prev.meta // {
     description = prev.meta.description + " (with xfgusta's colors patch)";
