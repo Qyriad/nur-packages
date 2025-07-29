2423f075da1e41c0187d7317ed5b2ea151528495
Qyriad <qyriad@qyriad.me>
2025-07-29 17:08:43 +0200

    strace-with-colors: update to Qyriad/v6.3-2

--- pkgs/strace-with-colors/package.nix
+++ pkgs/strace-with-colors/package.nix
@@ -1,16 +1,16 @@
 {
   strace,
-  fetchpatch,
+  fetchpatch2,
 }:
 
 # Strace patched to have colored output.
-strace.overrideAttrs (prev: {
-  patches = let
-    colorPatch = fetchpatch {
-      url = "https://raw.githubusercontent.com/xfgusta/strace-with-colors/v6.3-1/strace-with-colors.patch";
-      hash = "sha256-gcQldGsRgvGnrDX0zqcLTpEpchNEbCUFdKyii0wetEI=";
-    };
-  in prev.patches or [ ] ++ [ colorPatch ];
+strace.overrideAttrs (final: prev: {
+  # Based on the patch from https://github.com/xfgusta/strace-with-colors
+  colorPatch = fetchpatch2 {
+    url = "https://raw.githubusercontent.com/Qyriad/strace-with-colors/v6.3-2/strace-with-colors.patch";
+    hash = "sha256-PxzYx6BbiZfXgqlpQUWNugEvBulIXBHmo9eJp0+ylMI=";
+  };
+  patches = prev.patches or [ ] ++ [ final.colorPatch ];
 
   passthru.allowUnstructuredAttrs = true;
 
