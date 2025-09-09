3ef2004675062095c5e3d26f51be2a97b7a5d2af
Qyriad <qyriad@qyriad.me>
2025-09-09 23:32:18 +0200

    obs-studio-unwrapped: also link libsysprof-capture, rnnoise

--- pkgs/obs-studio-unwrapped/package.nix
+++ pkgs/obs-studio-unwrapped/package.nix
@@ -5,6 +5,8 @@
   obs-studio,
   libGL,
   libvlc,
+  libsysprof-capture,
+  rnnoise,
 }: let
   inherit (lib.mkPlatformGetters stdenv.hostPlatform)
     getLibrary;
@@ -21,6 +23,11 @@ in obs-studio.overrideAttrs (final: prev: {
       "$out/lib/obs-plugins/decklink.so"
     ];
   };
+
+  buildInputs = prev.buildInputs ++ [
+    libsysprof-capture
+    rnnoise
+  ];
 
   nativeBuildInputs = prev.nativeBuildInputs ++ [
     final.absoluteDylibsHook
