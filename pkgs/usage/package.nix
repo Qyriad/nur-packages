a215b14ccaaa3f597498b4c578e81bfe152952a2
Qyriad <qyriad@qyriad.me>
2025-12-31 14:11:57 +0100

    usage: 2.8.0 -> 2.11.0
    Diff: https://github.com/jdx/usage/compare/refs/tags/v2.8.0...refs/tags/v2.11.0

--- pkgs/usage/package.nix
+++ pkgs/usage/package.nix
@@ -13,7 +13,7 @@
 	inherit (lib.mkPlatformPredicates stdenv.hostPlatform) optionalDarwin;
 in stdenv.mkDerivation (self: {
 	pname = "usage";
-	version = "2.8.0";
+	version = "2.11.0";
 
 	strictDeps = true;
 	__structuredAttrs = true;
@@ -22,13 +22,13 @@ in stdenv.mkDerivation (self: {
 		owner = "jdx";
 		repo = "usage";
 		rev = "refs/tags/v${self.version}";
-		hash = "sha256-/yDypNQdw6YS1M8YtwjdFyG8Lfh3wKkvVWyH2b/G65o=";
+		hash = "sha256-AFfI843y1fKdw2f4alz7WoeMQR2IPWDJ3SofCCMJVpQ=";
 	};
 
 	cargoDeps = fetchCargoVendor {
 		name = "${self.finalPackage.name}-cargo-deps";
 		inherit (self) src;
-		hash = "sha256-3tSMgTVmoiME/wWE8uHZEjnfeS8Hqbm0DeUaWNgN944=";
+		hash = "sha256-WC/q9yd1XJT/EtC9ES5fw6j45gyRo3k2eNEDwGmvDWo=";
 	};
 
 	nativeBuildInputs = rustHooks.asList ++ [
@@ -51,8 +51,7 @@ in stdenv.mkDerivation (self: {
 		license = with lib.licenses; [ mit ];
 		sourceProvenance = with lib.sourceTypes; [ fromSource ];
 		platforms = lib.platforms.all;
-		# Rust 2024 edition was stablized in Rust 1.85.
-		broken = lib.versionOlder cargo.version "1.85.0";
+		broken = lib.versionOlder cargo.version "1.88.0";
 		mainProgram = "usage";
 	};
 }))
