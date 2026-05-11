b9c7b5b54890060a542e3f1e6fc8fb5807fb83b0
Qyriad <qyriad@qyriad.me>
2026-05-11 14:34:54 +0200

    lazycut: init at 0.3.9

---
+++ pkgs/lazycut/package.nix
@@ -0,0 +1,41 @@
+{
+	lib,
+	stdenv,
+	srcOnly,
+	fetchFromGitHub,
+	fetchGoModules,
+	goHooks,
+}: stdenv.mkDerivation (finalAttrs: let
+	self = finalAttrs.finalPackage;
+in {
+	pname = "lazycut";
+	version = "0.3.9";
+
+	doCheck = true;
+	doInstallCheck = true;
+
+	src = fetchFromGitHub {
+		owner = "ozemin";
+		repo = "lazycut";
+		tag = "v${self.version}";
+		hash = "sha256-SWcxk8GiXX81UZwv//1lukvXLtgMiJ7u4Rx1z6CKQoY=";
+	};
+
+	goModules = fetchGoModules {
+		name = lib.suffixName self "go-modules";
+		inherit (self) src;
+		hash = "sha256-KfVNSESu06xiFYb+r2Yv4rgDc/NZ1tuGC0IWUdQrywo=";
+	};
+
+	nativeBuildInputs = goHooks.asList;
+
+	passthru.srcs = srcOnly self;
+
+	meta = {
+		homepage = "https://github.com/ozemin/lazycut";
+		license = with lib.licenses; [ mit ];
+		maintainers = with lib.maintainers; [ qyriad ];
+		sourceProvenance = with lib.sourceTypes; [ fromSource ];
+		mainProgram = "lazycut";
+	};
+})
