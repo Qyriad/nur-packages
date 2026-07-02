53da954686c82fd092b15158dc14c3df96d0a956
Qyriad <qyriad@qyriad.me>
2026-07-02 20:33:19 +0200

    stng: 1.5.1 -> 1.7.0
    Diff: https://codeberg.org/atomdrift/stng/compare/v1.5.1...v1.7.0

--- pkgs/stng/package.nix
+++ pkgs/stng/package.nix
@@ -11,7 +11,7 @@
 	self = final.finalPackage;
 in {
 	pname = "stng";
-	version = "1.5.1";
+	version = "1.7.0";
 
 	strictDeps = true;
 	__structuredAttrs = true;
@@ -21,13 +21,13 @@ in {
 		owner = "atomdrift";
 		repo = "stng";
 		tag = "v${self.version}";
-		hash = "sha256-YOvvYeiR8VMTJ68g4PjQgph28KZJ7N/zknZRpC5iZiE=";
+		hash = "sha256-cIQV1pJ97XX4bhotVfDvFSABG6sU3/nL4o1S3cpKI7s=";
 	};
 
 	cargoDeps = fetchCargoVendor {
 		name = lib.suffixName self "cargo-deps";
 		inherit (self) src;
-		hash = "sha256-P/5p/+PVbQ9BoWKkBULI1fkD6FqZg6OQCZOaAy6p3Ow=";
+		hash = "sha256-0msK2XFFCNohe0sbrwZM7bl2DAnPcTO3n75l5Nobfbc=";
 	};
 
 	nativeBuildInputs = rustHooks.asList ++ [
