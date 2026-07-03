a489f8f05dc1344c6a71960a3f9e0272125dca35
Qyriad <qyriad@qyriad.me>
2026-07-03 17:25:10 +0200

    xontrib-abbrevs: refactor

--- pkgs/xontrib-abbrevs/package.nix
+++ pkgs/xontrib-abbrevs/package.nix
@@ -1,52 +1,58 @@
 {
 	lib,
-	python3,
+	stdenvNoCC,
 	fetchFromGitHub,
-}:
-
-let
-	inherit (python3.pkgs)
-		buildPythonPackage
-		setuptools
-		wheel
-		poetry-core
-		prompt-toolkit
-		setuptools-scm
-		xonsh
-	;
-
+	python3Packages,
+	pythonHooks,
+}: lib.callWith' python3Packages ({
+	python,
+	pytestCheckHook,
+	setuptools,
+	setuptools-scm,
+	xonsh,
+	prompt-toolkit,
+}: let
+	stdenv = stdenvNoCC;
+in stdenv.mkDerivation (finalAttrs: let
+	self = finalAttrs.finalPackage;
+in {
 	pname = "xontrib-abbrevs";
 	version = "0.1.1";
-in
-	buildPythonPackage {
-		inherit pname version;
 
-		src = fetchFromGitHub {
-			name = "${pname}-source";
-			owner = "xonsh";
-			repo = "xontrib-abbrevs";
-			rev = "refs/tags/v${version}";
-			hash = "sha256-xJUSbYo/+RFFCHenDEybVNxpOrEqSkU3eAaI+TNTmQI=";
-		};
+	__structuredAttrs = true;
+	strictDeps = true;
 
-		format = "pyproject";
+	doCheck = true;
+	doInstallCheck = true;
 
-		nativeBuildInputs = [
-			setuptools
-			wheel
-			poetry-core
-			setuptools-scm
-		];
+	outputs = [ "out" "dist" ];
 
-		nativeCheckInputs = [
-			xonsh
-			prompt-toolkit
-		];
+	src = fetchFromGitHub {
+		owner = "xonsh";
+		repo = "xontrib-abbrevs";
+		tag = "v${self.version}";
+		hash = "sha256-xJUSbYo/+RFFCHenDEybVNxpOrEqSkU3eAaI+TNTmQI=";
+	};
 
-		meta = {
-			description = "Xonsh extension for using direnv";
-			homepage = "https://github.com/74th/xonsh-direnv";
-			license = lib.licenses.mit;
-		};
+	nativeBuildInputs = (pythonHooks python).asList ++ [
+		setuptools
+		setuptools-scm
+	];
 
-	}
+	propagatedBuildInputs = [
+		xonsh
+		prompt-toolkit
+	];
+
+	nativeCheckInputs = [
+		pytestCheckHook
+	];
+
+	meta = {
+		description = "Xonsh extension for using direnv";
+		homepage = "https://github.com/74th/xonsh-direnv";
+		license = lib.licenses.mit;
+		maintainers = with lib.maintainers; [ qyriad ];
+		sourceProvenance = with lib.sourceTypes; [ fromSource ];
+	};
+}))
