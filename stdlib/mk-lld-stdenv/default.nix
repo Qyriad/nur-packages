911d445b72d518f45e6330e07357e514af76fd3e
Qyriad <qyriad@qyriad.me>
2026-07-07 19:30:55 +0200

    stdlib/mk-lld-stdenv: oops, merge, not override, cmake and meson flags

--- stdlib/mk-lld-stdenv/default.nix
+++ stdlib/mk-lld-stdenv/default.nix
@@ -9,15 +9,15 @@
 	bintools = llvmPackages.bintools;
 }
 |> stdenvAdapters.overrideCC llvmPackages.stdenv
-|> stdenvAdapters.addAttrsToDerivation {
+|> stdenvAdapters.addAttrsToDerivation (prev: {
 	mesonFlags = [
 		"-Dc_link_args=-fuse-ld=lld"
 		"-Dcpp_link_args=-fuse-ld=lld"
-	];
+	] ++ prev.mesonFlags or [ ];
 	cmakeFlags = [
 		"-DCMAKE_EXE_LINKER_FLAGS_INIT=-fuse-ld=lld"
 		"-DCMAKE_SHARED_LINKER_FLAGS_INIT=-fuse-ld=lld"
 		"-DCMAKE_STATIC_LINKER_FLAGS_INIT=-fuse-ld=lld"
 		"-DCMAKE_MODULE_LINKER_FLAGS_INIT=-fuse-ld=lld"
-	];
-}
+	] ++ prev.cmakeFlags or [ ];
+})
