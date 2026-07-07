961d097257d20f589c9a4f5920511ae46e9ba673
Qyriad <qyriad@qyriad.me>
2026-07-07 18:16:50 +0200

    strace-process-tree: refactor

--- pkgs/strace-process-tree/package.nix
+++ pkgs/strace-process-tree/package.nix
@@ -1,49 +1,50 @@
 {
 	lib,
+	stdenvNoCC,
+	stdlib,
 	fetchFromGitHub,
-	python3,
-}:
-
-let
-	inherit (python3.pkgs)
-		buildPythonApplication
-		setuptools
-		wheel
-		pytest
-	;
-
+	python3Packages,
+	pythonHooks,
+}: lib.callWith' python3Packages ({
+	python,
+	setuptools,
+	pytest,
+	pytestCheckHook,
+	pythonImportsCheckHook,
+}: let
+	stdenv = stdenvNoCC;
+in stdlib.makePackage stdenv (finalAttrs: let
+	self = finalAttrs.finalPackage;
+in {
 	pname = "strace-process-tree";
 	version = "1.5.3";
-in
-	buildPythonApplication {
-		inherit pname version;
 
-		__structuredAttrs = true;
-		strictDeps = true;
+	outputs = [ "out" "dist" ];
 
-		src = fetchFromGitHub {
-			owner = "mgedmin";
-			repo = "strace-process-tree";
-			rev = version;
-			sha256 = "sha256-yya8mL7aRl0KvkhyIRpah8DPLbQ75JX1rutV0/tOVHU=";
-			name = "${pname}-source";
-		};
+	doCheck = true;
+	doInstallCheck = true;
 
-		format = "pyproject";
+	src = fetchFromGitHub {
+		owner = "mgedmin";
+		repo = "strace-process-tree";
+		tag = "${self.version}";
+		sha256 = "sha256-yya8mL7aRl0KvkhyIRpah8DPLbQ75JX1rutV0/tOVHU=";
+	};
 
-		nativeBuildInputs = [
-			setuptools
-			wheel
-		];
+	nativeBuildInputs = (pythonHooks python).asList ++ [
+		setuptools
+	];
 
-		nativeCheckInputs = [
-			pytest
-		];
+	nativeCheckInputs = [
+		pytestCheckHook
+	];
 
-		meta = {
-			mainProgram = "strace-process-tree";
-			description = "Tool to help me make sense of `strace -f` output";
-			homepage = "https://github.com/mgedmin/strace-process-tree";
-			license = lib.licenses.gpl2;
-		};
-	}
+	meta = {
+		mainProgram = "strace-process-tree";
+		description = "Tool to help me make sense of `strace -f` output";
+		homepage = "https://github.com/mgedmin/strace-process-tree";
+		license = lib.licenses.gpl2;
+		maintainers = with lib.maintainers; [ qyriad ];
+		sourceProvenance = with lib.sourceTypes; [ fromSource ];
+	};
+}))
