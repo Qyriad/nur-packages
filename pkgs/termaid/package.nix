44f8e182e8cf96855c084fc1f6a5c2db95d0ea09
Qyriad <qyriad@qyriad.me>
2026-08-08 14:24:39 +0200

    termaid: init at 0.9.0

---
+++ pkgs/termaid/package.nix
@@ -0,0 +1,45 @@
+{
+	lib,
+	stdlib,
+	stdenv,
+	fetchFromGitHub,
+	pythonHooks,
+	python3Packages,
+	versionCheckHook,
+}: lib.callWith' python3Packages ({
+	python,
+	hatchling,
+}: stdlib.makePackage stdenv (finalAttrs: let
+	self = finalAttrs.finalPackage;
+in {
+	pname = "termaid";
+	version = "0.9.0";
+
+	outputs = [ "out" "dist" ];
+
+	src = fetchFromGitHub {
+		owner = "fasouto";
+		repo = "termaid";
+		tag = "v${self.version}";
+		hash = "sha256-PIpciXaiduwBOvDcbEglywfFFZZ7bdEAMuT8vac8oRQ=";
+	};
+
+	nativeBuildInputs = (pythonHooks python).asList ++ [
+		hatchling
+	];
+
+	nativeInstallCheckInputs = [
+		versionCheckHook
+	];
+
+	postFixupHooks = [ "wrapPythonPrograms" ];
+
+	meta = {
+		homepage = "https://github.com/fasouto/termaid";
+		description = "";
+		maintainers = with lib.maintainers; [ qyriad ];
+		license = with lib.licenses; [ mit ];
+		sourceProvenance = with lib.sourceTypes; [ fromSource ];
+		mainProgram = "termaid";
+	};
+}))
