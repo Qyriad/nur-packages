f5bb76c4b39398d9f241c164b338ca5900441381
Qyriad <qyriad@qyriad.me>
2026-08-27 14:55:22 +0200

    mk-lld-stdenv: fix `ar` usage under cmake, oops~

--- stdlib/mk-lld-stdenv/default.nix
+++ stdlib/mk-lld-stdenv/default.nix
@@ -17,7 +17,6 @@
 	cmakeFlags = [
 		"-DCMAKE_EXE_LINKER_FLAGS_INIT=-fuse-ld=lld"
 		"-DCMAKE_SHARED_LINKER_FLAGS_INIT=-fuse-ld=lld"
-		"-DCMAKE_STATIC_LINKER_FLAGS_INIT=-fuse-ld=lld"
 		"-DCMAKE_MODULE_LINKER_FLAGS_INIT=-fuse-ld=lld"
 	] ++ prev.cmakeFlags or [ ];
 })
