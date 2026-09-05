b2b2a4c8ce7e340841249ec03e248088733c9821
Qyriad <qyriad@qyriad.me>
2026-09-05 16:24:59 +0200

    usage: 3.5.6 -> 6.6.1
    Diff: https://github.com/jdx/usage/compare/refs/tags/v3.5.6...v6.6.1

--- pkgs/usage/package.nix
+++ pkgs/usage/package.nix
@@ -16,7 +16,7 @@ in stdlib.makePackage stdenv (finalAttrs: let
 	self = finalAttrs.finalPackage;
 in {
 	pname = "usage";
-	version = "3.5.6";
+	version = "6.6.1";
 
 	# Some of the tests rely on `usage` in PATH. We'll fix those later.
 	dontCargoCheck = true;
@@ -25,13 +25,13 @@ in {
 		owner = "jdx";
 		repo = "usage";
 		rev = "refs/tags/v${self.version}";
-		hash = "sha256-/57P3XC/7z6Ul03ENCzyHhknmsTJwkJTfIf61rIXy5Y=";
+		hash = "sha256-OQCIQqR8tO10w1WTi6rPCdBN5o9bZetmwGojU3p4o4I=";
 	};
 
 	cargoDeps = fetchCargoVendor {
 		name = lib.suffixName self "cargo-deps";
 		inherit (self) src;
-		hash = "sha256-Z4ey6khCcKJdJhFVcEdvyhd6szRH0QC0Z5hifjMxE48=";
+		hash = "sha256-dZo75rg3kh4R7D79A1JOYnq0s8iWzf1bJffm3oK7I2A=";
 	};
 
 	nativeBuildInputs = rustHooks.asList ++ [
