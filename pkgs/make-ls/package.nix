bc4e6b7f6be5e77834143e16e5d225631ee7cd74
Qyriad <qyriad@qyriad.me>
2026-09-05 16:23:58 +0200

    make-ls: 0.1.16 -> 0.1.20
    Diff: https://github.com/owenrumney/make-ls/compare/v0.1.16...v0.1.20

--- pkgs/make-ls/package.nix
+++ pkgs/make-ls/package.nix
@@ -9,19 +9,19 @@
 	self = finalAttrs.finalPackage;
 in {
 	pname = "make-ls";
-	version = "0.1.16";
+	version = "0.1.20";
 
 	src = fetchFromGitHub {
 		owner = "owenrumney";
 		repo = "make-ls";
 		tag = "v${self.version}";
-		hash = "sha256-pgLK84+eriAPDeteWQUXXvN9zM3d8dn/J6zWdrPOtVc=";
+		hash = "sha256-tnOiPKl8ULN2+fcEqdXgsRGTlbR2y2lDicaHyFxQkFs=";
 	};
 
 	goModules = fetchGoModules {
 		name = lib.suffixName self "go-modules";
 		inherit (self) src;
-		hash = "sha256-7QwDZZ8MKL210z/Bs8DKkRhj/Ju63yYQFlqQ2zJI5OE=";
+		hash = "sha256-HJYqbYDN7HslR4Zar2JnO/xbwgKFffH8tMrUAHYnzFU=";
 	};
 
 	nativeBuildInputs = goHooks.asList ++ [
